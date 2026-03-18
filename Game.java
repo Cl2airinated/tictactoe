3703b1c13a2e90b6a94a788268845e6246b78183
Cl2airinated <164077265+Cl2airinated@users.noreply.github.com>
2026-03-18 19:17:31 +0000

    init

---
+++ Game.java
@@ -0,0 +1,184 @@
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent; 
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+
+public class Game  extends JPanel implements Runnable, KeyListener, MouseListener, MouseMotionListener{
+
+	
+	private BufferedImage back; 
+	private int key; 
+	private int row, column;
+	char[][] board = new char[2][2];
+	private String x,o;
+
+	
+	public Game() {
+		new Thread(this).start();	
+		this.addKeyListener(this);
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+		key =-1; 
+		
+		
+	
+	}
+
+	
+	
+	public void run()
+	   {
+	   	try
+	   	{
+	   		while(true)
+	   		{
+	   		   Thread.currentThread().sleep(5);
+	            repaint();
+	         }
+	      }
+	   		catch(Exception e)
+	      {
+	      }
+	  	}
+	
+
+	
+	
+	
+	public void paint(Graphics g){
+		
+		Graphics2D twoDgraph = (Graphics2D) g; 
+		if( back ==null)
+			back=(BufferedImage)( (createImage(getWidth(), getHeight()))); 
+		
+
+		Graphics g2d = back.createGraphics();
+	
+		g2d.clearRect(0,0,getSize().width, getSize().height);
+	
+		g2d.setFont( new Font("Broadway", Font.BOLD, 50));
+		
+		
+		
+	
+		twoDgraph.drawImage(back, null, 0, 0);
+
+	}
+
+	
+	public void initializeBaord(Graphics g){
+		for(int i = 0; i < 2; i ++){
+    		for(int j = 0; j < 2; j ++){
+				System.out.print(board);
+    }
+    
+}
+	}
+
+
+	//DO NOT DELETE
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+//DO NOT DELETE
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		key= e.getKeyCode();
+		System.out.println(key);
+		
+		
+		
+	
+	}
+
+
+	//DO NOT DELETE
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+		
+		
+		
+	}
+
+
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("entered");
+	}
+
+
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("exited");
+	}
+
+
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("you clicked at"+ arg0.getY());
+		
+		
+	}
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+	
+}
